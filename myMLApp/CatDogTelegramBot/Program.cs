71aed9d3167be946a9189c68408f94b746d19af3
Ilya Parakhin <thenitro1221.good@gmail.com>
2024-05-21 06:52:34 +0300

    done.

--- myMLApp/CatDogTelegramBot/Program.cs
+++ myMLApp/CatDogTelegramBot/Program.cs
@@ -5,8 +5,10 @@ using Newtonsoft.Json;
 using Telegram.Bot.Polling;
 using Telegram.Bot.Exceptions;
 using System.Globalization;
+using Telegram.Bot.Types.Enums;
+using Telegram.Bot.Types;
 
-internal class Program
+internal static class Program
 {
     private static void Main(string[] args)
     {
@@ -49,54 +51,88 @@ internal class Program
         if (message.Date.ToUniversalTime() >= localTime.AddMinutes(-1).ToUniversalTime() &&
         message.Date.ToUniversalTime() <= localTime.AddMinutes(1).ToUniversalTime())
         {
-            Console.WriteLine($"SendTime (UTF): {message.Date}\nLocalTime: {localTime}\nMessage: {JsonConvert.SerializeObject(update)}");
-            if (message.Text != null)
+            _ = Task.Run(async () =>
             {
-                if (message.Text == @"/start")
+                Console.WriteLine($"SendTime (UTF): {message.Date}\nLocalTime: {localTime}\nMessage: {JsonConvert.SerializeObject(update)}");
+                if (message.Text != null)
                 {
-                    await client.SendTextMessageAsync(message.Chat.Id, "Welcome!\nThis bot detects whether it is a cat or dog on a picture.\n" +
-                        "Supported formats are: .jpeg, .jpg, .png, .gif.\nPlease, send picture as documents/files." );
+                    if (message.Text == @"/start")
+                    {
+                        await client.SendTextMessageAsync(message.Chat.Id, "Welcome!\nThis bot detects whether it is a cat or dog on a picture.\n" +
+                            "Supported formats are: .jpeg, .jpg, .png, .gif.\nPlease, send picture as documents/files.");
+                        return;
+                    }
                     return;
                 }
-                return;
-            }
-            if (message.Photo != null) 
-            {
-                await client.SendTextMessageAsync(message.Chat.Id, "Please, send picture as document.");
-                return;
-            }
-            if (message.Document != null)
-            {
-                if (message.Document.FileName == null) return;
-                if (!IsPhotoByExtension(message.Document.FileName))
+                if (message.Photo != null)
                 {
-                    await client.SendTextMessageAsync(message.Chat.Id, "Wrong file format!\n" +
-                            "Supported formats are: .jpeg, .jpg, .png, .gif.\nPlease, send picture as documents/files.");
+                    await client.SendTextMessageAsync(message.Chat.Id, "Please, send picture as document.");
+                    return;
                 }
-                var fileID = message.Document.FileId;
-                var fileInfo = await client.GetFileAsync(fileID);
-                var filePath = fileInfo.FilePath;
-                if (!Directory.Exists(@$"{Environment.CurrentDirectory}\DownloadedFiles\)"))
+                if (message.Document != null)
                 {
-                    Directory.CreateDirectory(@$"{Environment.CurrentDirectory}\DownloadedFiles\");
-                    Console.WriteLine("Directory created.");
+                    if (message.Document.FileName == null) return;
+                    if (!IsPhotoByExtension(message.Document.FileName))
+                    {
+                        await client.SendTextMessageAsync(message.Chat.Id, "Wrong file format!\n" +
+                                "Supported formats are: .jpeg, .jpg, .png, .gif.\nPlease, send picture as documents/files.");
+                    }
+                    var fileID = message.Document.FileId;
+                    var fileInfo = await client.GetFileAsync(fileID);
+                    var filePath = fileInfo.FilePath;
+                    if (!Directory.Exists(@$"{Environment.CurrentDirectory}\DownloadedFiles\)"))
+                    {
+                        Directory.CreateDirectory(@$"{Environment.CurrentDirectory}\DownloadedFiles\");
+                        Console.WriteLine("Directory created.");
+                    }
+                    string destinationFilePath = @$"{Environment.CurrentDirectory}\DownloadedFiles\{message.Document.FileName}";
+                    await using Stream fileStream = System.IO.File.OpenWrite(destinationFilePath);
+                    await client.DownloadFileAsync(filePath!, fileStream);
+                    fileStream.Close();
+                    var sortedScoresWithLabel = await Task.Run(() => CatRecognizerModel.processPictureWithModel(destinationFilePath));
+                    System.IO.File.Delete(destinationFilePath);
+                    foreach (var score in sortedScoresWithLabel)
+                    {
+                        string formattedValue = score.Value.ToString("F2", CultureInfo.InvariantCulture);
+                        //await Task.Delay(5000);
+                        await client.SendTextMessageAsync(message.Chat.Id, $"{score.Key}: {formattedValue}%");
+                    }
+                    return;
                 }
-                string destinationFilePath = @$"{Environment.CurrentDirectory}\DownloadedFiles\{message.Document.FileName}";
-                await using Stream fileStream = System.IO.File.OpenWrite(destinationFilePath);
-                await client.DownloadFileAsync(filePath!, fileStream);
-                fileStream.Close();
-                var sortedScoresWithLabel = await Task.Run(() => CatRecognizerModel.processPictureWithModel(destinationFilePath));
-                System.IO.File.Delete(destinationFilePath);
-                foreach (var score in sortedScoresWithLabel)
-                {
-                    string formattedValue = score.Value.ToString("F2", CultureInfo.InvariantCulture);
-                    await client.SendTextMessageAsync(message.Chat.Id, $"{score.Key}: {formattedValue}%");
-                }
-                return;
-            }
+            });
         }
         return;
     }
+
+    /*private static async Task HandleUpdateAsync(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
+    {
+        // Run each update handling in a separate task to ensure concurrency
+        _ = Task.Run(async () =>
+        {
+            try
+            {
+                if (update.Type == UpdateType.Message && update.Message.Type == MessageType.Text)
+                {
+                    var chatId = update.Message.Chat.Id;
+                    var messageText = update.Message.Text;
+                    Console.WriteLine($"Received a message from {chatId}: {messageText}");
+
+                    // Simulate a time-consuming operation with a delay
+                    await Task.Delay(5000); // 5 seconds delay
+
+                    // Echo the received message back to the user after the delay
+                    await botClient.SendTextMessageAsync(
+                        chatId: chatId,
+                        text: $"You said: {messageText}",
+                        cancellationToken: cancellationToken);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error processing update: {ex.Message}");
+            }
+        });
+    }*/
 
     static bool IsPhotoByExtension(string filePath)
     {
